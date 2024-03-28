bba11e563c7f953258abc58b9bd66f14e1f71a27
erray <a.erray@itroadgroup.com>
2024-03-28 23:42:43 +0000

    AuthController is updated

--- src/main/java/com/mobi/mobe/controller/AuthController.java
+++ src/main/java/com/mobi/mobe/controller/AuthController.java
@@ -1,5 +1,6 @@
 package com.mobi.mobe.controller;
 
+import com.mobi.mobe.dto.UserDTO;
 import com.mobi.mobe.dto.request.AuthenticationRequest;
 import com.mobi.mobe.dto.request.RegisterRequest;
 import com.mobi.mobe.dto.response.AuthenticationResponse;
@@ -8,10 +9,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 
@@ -38,10 +36,15 @@ public class AuthController {
         return ResponseEntity.ok("Logged out successfully");
     }
 
-
     @PostMapping("/refresh-token")
     public void refreshToken(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
         authenticationService.refreshToken(request, response);
+    }
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<UserDTO> getUserById(@PathVariable Long userId) {
+        UserDTO userOptional = authenticationService.getUserById(userId);
+        return ResponseEntity.ok(userOptional);
     }
 }
