1bf39cad7b8bd2f3890234d304dda37f12c86e5c
erray <a.erray@itroadgroup.com>
2024-03-28 23:46:41 +0000

    LikeDTO is updated

--- src/main/java/com/mobi/mobe/dto/LikeDTO.java
+++ src/main/java/com/mobi/mobe/dto/LikeDTO.java
@@ -1,4 +1,16 @@
 package com.mobi.mobe.dto;
 
+import com.mobi.mobe.entities.User;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class LikeDTO {
+    private Long id;
+    private Long userId;
 }
