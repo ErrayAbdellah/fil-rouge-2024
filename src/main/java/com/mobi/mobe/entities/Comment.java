063edb3e450f8ce1af2277f1bfc359ead5b86877
erray <a.erray@itroadgroup.com>
2024-03-29 23:52:17 +0000

    Comment is updated

--- src/main/java/com/mobi/mobe/entities/Comment.java
+++ src/main/java/com/mobi/mobe/entities/Comment.java
@@ -3,7 +3,6 @@ package com.mobi.mobe.entities;
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.*;
 import lombok.*;
-import org.antlr.v4.runtime.misc.NotNull;
 import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.LocalDateTime;
@@ -22,23 +21,18 @@ public class Comment {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
-    @NotNull
     @Column(name = "content", nullable = false)
-    @CreationTimestamp
     private String content;
 
-    @NotNull
     @Column(name = "created_at", nullable = false)
     @CreationTimestamp
     private LocalDateTime createdAt;
 
-    @NotNull
     @Column(name = "updated_at", nullable = false)
     @CreationTimestamp
     private LocalDateTime updatedAt;
 
-    @NotNull
-    @Column(name = "url_content",nullable = false)
+    @Column(name = "url_content")
     private String urlContent;
 
     @ManyToOne
