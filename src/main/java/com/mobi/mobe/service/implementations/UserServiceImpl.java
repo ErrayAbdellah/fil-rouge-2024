02bea30dc5781ee34055a7544e3239feae322f86
erray <a.erray@itroadgroup.com>
2024-04-01 23:59:09 +0000

    changeloge liquibase

--- src/main/java/com/mobi/mobe/service/implementations/UserServiceImpl.java
+++ src/main/java/com/mobi/mobe/service/implementations/UserServiceImpl.java
@@ -1,6 +1,8 @@
 package com.mobi.mobe.service.implementations;
 
 import com.mobi.mobe.service.UserService;
+import org.springframework.stereotype.Component;
 
+@Component
 public class UserServiceImpl implements UserService {
 }
